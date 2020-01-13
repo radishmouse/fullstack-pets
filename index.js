c2f6b1e92eab5a21f6f8f6b82b1943f47ae29294
Chris Aquino <chrisaquino@radishmouse.local>
2020-01-13 14:56:17 -0500

    add sample routes for exercise

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+const server = http.createServer(app);
+
 const pets = require('./models/pets');
 
-// pets.all()
-//     .then(allThePets => {
-//         // do stuff
-//         console.log()
-//     })
 
-async function main() {
-    // const thePets = await pets.all();
-    // console.log(thePets);
-    // const aPet = await pets.one(1);
-    // console.log(aPet);
+// See all pets!
+app.get('/pets', async (req, res) => {
+    const thePets = await pets.all();
+    // res.send('you want /pets');
+    res.json(thePets);
 
-    // const result = await pets.del(1);
-    // console.log(result);
-    // const updateResult = await pets.updateName(1, 'the amazing oakley');
-    // console.log(updateResult);
-    // const updateResult = await pets.updateBirthdate(1, new Date());
-    // console.log(updateResult);
+    // Here's what `res.json()` is doing:
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/json'
+    // })
+    // const jsonString = JSON.stringify(thePets);
+    // res.write(jsonString);
+    // res.end();
+});
 
-    const createResult = await pets.create('billy', 'goat', '2020-01-13', 1);
-    console.log(createResult);
-}
+app.get('/pets/:id')
 
-main();
+// Create
+app.get('/pets/create')
+app.post('/pets/create')
+
+// Update
+app.get('/pets/:id/edit')
+app.post('/pets/:id/edit')
+
+// Delete
+app.get('/pets/:id/delete')
+app.post('/pets/:id/delete')
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
